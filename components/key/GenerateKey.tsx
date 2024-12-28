02c97e8963f025ef71748f130ce6656a95b5a8d8
Adarsh Dhar <dharadarsh0@gmail.com>
2024-12-28 09:40:08 +0530

    keys getting stored

--- components/key/GenerateKey.tsx
+++ components/key/GenerateKey.tsx
@@ -1,12 +1,12 @@
-import { MaciKeyGenerator } from "../lib/generate";
-import { KeyStorage } from "../lib/store";
+
 import { useState } from "react";
 import { Button } from "../ui/button";
-import { toast } from "sonner";
+import StoreKeys from "./StoreKey";
 import { buildEddsa } from "circomlibjs";
 import crypto from 'crypto';
 
 export function GenerateKey() {
+    const {storeKeys, getKeys} = StoreKeys()
     const [newKeyName, setNewKeyName] = useState('');
     const [loading, setLoading] = useState(false);
 
@@ -14,47 +14,15 @@ export function GenerateKey() {
         const eddsa = await buildEddsa();
         const prvkey = crypto.randomBytes(32)
         const pubkey = eddsa.prv2pub(prvkey)
+        const finalPubKey = Buffer.concat([pubkey[0], pubkey[1]]);
         console.log("private key ",prvkey);
         console.log("public key ",pubkey)
+        console.log("final public key ",finalPubKey)
+        await storeKeys(finalPubKey,prvkey)
+        const get_keys = await getKeys()
+        console.log("get keys ",get_keys)
     }
 
-    const handleGenerateKey = async () => {
-        console.log("hello")
-        // if (!newKeyName) {
-        //     console.log("no name")
-        //     toast.error("Please enter a key name");
-        //     return;
-        // }
-
-        try {
-            setLoading(true);
-            console.log("Initializing key generator...");
-            // Initialize key generator
-            const keyGenerator = await MaciKeyGenerator.initialize();
-            console.log("Key generator initialized:", keyGenerator);
-            
-            console.log("Generating key pair...");
-            // Generate key pair
-            const keyPair = await keyGenerator.generateKeyPair();
-            console.log("Key pair generated:", keyPair);
-            
-            console.log("Storing key...");
-            // Store the key
-            KeyStorage.storeKey(keyPair, newKeyName);
-            console.log("Key stored:", newKeyName);
-            // Clear input and show success message
-            setNewKeyName('');
-            toast.success("Key generated successfully");
-            
-        } catch (error) {
-            console.error('Key generation error:', error);
-            toast.error("Failed to generate key");
-        } finally {
-            setLoading(false);
-            console.log("yo")
-        }
-        console.log("bye")
-    };
 
     return(
         <div>
